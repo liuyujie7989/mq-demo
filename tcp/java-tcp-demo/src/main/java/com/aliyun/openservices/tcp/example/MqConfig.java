7eaee71bd5b7abe6a7c906d3f0f2d6dd7b471ecc
liuyujie <liuyujie123>
2020-03-26 16:47:01 +0800

    阿里云mq配置更改

--- tcp/java-tcp-demo/src/main/java/com/aliyun/openservices/tcp/example/MqConfig.java
+++ tcp/java-tcp-demo/src/main/java/com/aliyun/openservices/tcp/example/MqConfig.java
@@ -22,17 +22,29 @@ public class MqConfig {
     /**
      * 启动测试之前请替换如下 XXX 为您的配置
      */
-    public static final String TOPIC = "XXX";
-    public static final String GROUP_ID = "XXX";
-    public static final String ORDER_TOPIC = "XXX";
-    public static final String ORDER_GROUP_ID = "XXX";
-    public static final String ACCESS_KEY = "XXX";
-    public static final String SECRET_KEY = "XXX";
-    public static final String TAG = "mq_test_tag";
+    //测试环境
+   public static final String TOPIC = "DEVZT_ZT_AUTHORITY_DATASEND_TOPIC";
+     public static final String Consumer_Id = "CID_ACCEPT_ORG_USER";
+    public static final String Producer_Id = "PID_DEVZT_ZT_AUTHORITY_DATASEND";
+    public static final String ORDER_TOPIC = "DEVZT_ZT_AUTHORITY_DATASEND_TOPIC";
+    public static final String ORDER_GROUP_ID = "AliyunMQ";
+    public static final String ACCESS_KEY = "z760kkGR0MTXsJ76";
+    public static final String SECRET_KEY = "YyVNR6wp0YGOd36RwUabeqtUoOOKK2";
+    public static final String TAG = "*";
+//生产环境
+ /*   public static final String TOPIC = "CITY_PROD_AUTHORITY_DATASEND_TOPIC";
+    public static final String Consumer_Id = "CID_CITY_PROD_AUTHORITY_DATASEND";
+    public static final String Producer_Id = "PID_CITY_PROD_AUTHORITY_DATASEND";
+    public static final String ORDER_TOPIC = "CITY_PROD_AUTHORITY_DATASEND_TOPIC";
+    public static final String ORDER_GROUP_ID = "AliyunMQ";
+    public static final String ACCESS_KEY = "Sm0h9kgdDMaoTJy0";
+    public static final String SECRET_KEY = "SfbojalUpxUZfM3G8tn2V04BOgxO6I";
+    public static final String TAG = "*";*/
+
 
     /**
      * NAMESRV_ADDR, 请在mq控制台 https://ons.console.aliyun.com 通过"实例管理--获取接入点信息--TCP协议接入点"获取
      */
-    public static final String NAMESRV_ADDR = "XXX";
+    public static final String NAMESRV_ADDR = "10.16.11.37:9876";
 
 }
